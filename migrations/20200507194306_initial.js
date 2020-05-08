6ff8408295c13780dac3243fc76a0db7c9b46485
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-07 19:49:14 -0600

    Used the migrations to create the users table with columns for username, password, and department.

--- migrations/20200507194306_initial.js
+++ migrations/20200507194306_initial.js
@@ -1,8 +1,12 @@
+exports.up = async function(knex) {
+	await knex.schema.createTable("users", (table) => {
+		table.increments()
+		table.text("username").notNull().unique()
+        table.text("password").notNull()
+        table.text("department")
+	})
+}
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = async function(knex) {
+	await knex.schema.dropTableIfExists("users")
+}
